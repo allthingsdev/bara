5a230c86b2645db14d641a2864a71d0c5a3f83f8
allthingsdev <allthingsdev2017@gmail.com>
2019-01-25 13:24:01 +0300

    Email Registration - Update RegistrationController.java, User.java, and register.html

--- src/main/java/mg/tmr/khr/bara/main/model/User.java
+++ src/main/java/mg/tmr/khr/bara/main/model/User.java
@@ -40,11 +40,9 @@ public class User {
 	private String password;
 	
 	@Column(name = "first_name")
-	@NotEmpty(message = "Please provide your first name")
 	private String firstName;
 	
 	@Column(name = "last_name")
-	@NotEmpty(message = "Please provide your last name")
 	private String lastName;
 	
 	@Size(max = 255)
